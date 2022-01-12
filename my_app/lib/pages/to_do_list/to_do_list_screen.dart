b0364f2d404d379bab22c153269ea0f0815aef28
Mira Nguyen <mira.nguyen@nexcel.vn>
2022-01-12 20:41:29 +0700

    edit screen

--- my_app/lib/pages/to_do_list/to_do_list_screen.dart
+++ my_app/lib/pages/to_do_list/to_do_list_screen.dart
@@ -14,26 +14,26 @@ class ToDoListScreen extends StatefulWidget {
 }
 
 class _ToDoListScreenState extends State<ToDoListScreen> {
-  late TextEditingController _taskIdController = TextEditingController();
-  late TextEditingController _taskTextController = TextEditingController();
-  late TextEditingController _noteController = TextEditingController();
+  late final TextEditingController _taskIdController = TextEditingController();
+  late final TextEditingController _taskTextController =
+      TextEditingController();
+  late final TextEditingController _noteController = TextEditingController();
 
   final List<TaskModel> taskLists = [
     TaskModel('1', 'note 1', 'Task 1'),
     TaskModel('2', 'note 2', 'Task 2'),
     TaskModel('3', 'note 3', 'Task 3'),
-    TaskModel('4', 'note 4', 'Task 4'),
-    TaskModel('5', 'note 5', 'Task 5'),
-    TaskModel('6', 'note 6', 'Task 6'),
+    TaskModel('4', 'note 4', 'Task 4')
   ];
   late String _mode = 'taskView';
+  late TaskModel _task;
 
   Widget cardWidget(TaskModel task, BuildContext context) {
     return Card(
         clipBehavior: Clip.antiAlias,
         child: Column(children: [
           ListTile(
-            leading: Text('${task.taskId}'),
+            leading: Text(task.taskId),
             title: Text(task.task),
             subtitle: Text(
               task.note,
@@ -42,8 +42,12 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
             trailing: Wrap(
               children: [
                 IconButton(
-                    onPressed: () =>
-                        context.read<TodoBloc>().add(EditTaskEvent(task)),
+                    onPressed: () {
+                      setState(() {
+                        _mode = 'edit';
+                        _task = task;
+                      });
+                    },
                     icon: const Icon(
                       Icons.edit,
                       color: Color(0xFFEF5350),
@@ -92,19 +96,19 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
         padding: const EdgeInsets.all(10),
         child: Column(
           children: [
-            TextField(
+            TextFormField(
               controller: _taskIdController,
               decoration: const InputDecoration(
                 labelText: 'TaskID',
               ),
             ),
-            TextField(
+            TextFormField(
               controller: _taskTextController,
               decoration: const InputDecoration(
                 labelText: 'Task',
               ),
             ),
-            TextField(
+            TextFormField(
               controller: _noteController,
               decoration: const InputDecoration(
                 labelText: 'Note',
@@ -131,13 +135,63 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
         ));
   }
 
+  Widget editTask(taskLists, BuildContext context, TaskModel task) {
+    String _newTask = task.task;
+    String _newNote = task.note;
+    String _newId = task.taskId;
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    return Container(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                TextFormField(
+                  initialValue: task.taskId,
+                  decoration: const InputDecoration(
+                    labelText: 'TaskID',
+                  ),
+                  onSaved: (newValue) => _newId = newValue ?? '',
+                ),
+                TextFormField(
+                    initialValue: task.task,
+                    decoration: const InputDecoration(
+                      labelText: 'Task',
+                    ),
+                    onSaved: (newValue) => _newTask = newValue ?? ''),
+                TextFormField(
+                    initialValue: task.note,
+                    decoration: const InputDecoration(
+                      labelText: 'Note',
+                    ),
+                    onSaved: (newValue) => _newNote = newValue ?? ''),
+                TextButton(
+                  style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green)),
+                  onPressed: () {
+                    _formKey.currentState?.save();
+                    context.read<TodoBloc>().add(
+                        EditTaskEvent(TaskModel(_newId, _newNote, _newTask)));
+                    setState(() {
+                      _mode = 'taskView';
+                    });
+                  },
+                  child: const Text('Add task'),
+                ),
+              ],
+            )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => TodoBloc(taskLists),
       child: BlocBuilder<TodoBloc, TodoState>(
           builder: (context, state) => _mode == 'edit'
-              ? Container(child: listCard(state.taskLists, context))
+              ? Container(child: editTask(state.taskLists, context, _task))
               : (_mode == 'add'
                   ? Container(child: addTask(state.taskLists, context))
                   : Container(child: listCard(state.taskLists, context)))),
